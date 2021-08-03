a004518da708e5ad7cef6d40624da932d19112b2
Yatmanov Evgeniy <64483257+moizhirok@users.noreply.github.com>
2021-08-03 22:55:28 +0300

    import: update notice import

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,21 +1,21 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import showCase from "../components/ShowCase/ShowCase.slice";
-import notice from "../components/Notice/Notivce.slice";
+import notice from "../components/Notice/Notice.slice";
 import searchValue from "../components/Search/Search.slice";
 
 export const store = configureStore({
-  reducer: {
-    showCase,
-    notice,
-    searchValue,
-  },
+	reducer: {
+		showCase,
+		notice,
+		searchValue,
+	},
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
+	ReturnType,
+	RootState,
+	unknown,
+	Action<string>
 >;
